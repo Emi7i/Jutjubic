320c8975d35a7299b74ae0c323ca744790487500
Emi7i <emilijaopsenica@gmail.com>
2026-01-11 23:59:39 +0100

    fix

--- backend/src/main/java/isa/jutjub/model/User.java
+++ backend/src/main/java/isa/jutjub/model/User.java
@@ -1,7 +1,11 @@
 package isa.jutjub.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
@@ -19,16 +23,7 @@ public class User {
     @Column(nullable = false)
     private String role = "USER"; // simple role
 
-    // getters and setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    @Column(nullable = false)
+    private String email = "USER";
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
 }
