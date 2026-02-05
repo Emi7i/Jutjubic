56cc7696df3ff2fc4af9533db00ec1ffca4b0f4b
Emi7i <emilijaopsenica@gmail.com>
2026-02-06 00:22:02 +0100

    refine: front

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HomeComponent } from './components/home/home.component';
 import { VideoUploadComponent } from './components/video-upload/video-upload.component';
 import { VideoListComponent } from './components/video-list/video-list.component';
 import { VideoDetailComponent } from './components/video-detail/video-detail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { VideoService } from './services/video.service';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -30,6 +31,7 @@ import { ActivateComponent } from './pages/activate/activate.component';
     VideoUploadComponent,
     VideoListComponent,
     VideoDetailComponent,
+    NavbarComponent,
     RegisterFormComponent,
     RegisterComponent,
     LoginFormComponent,
